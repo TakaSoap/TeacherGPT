9051b109ba318b1c488a5352ff8e8d4431cdebd0
TakaSoap <34995160+TakaSoap@users.noreply.github.com>
2023-04-04 18:33:38 +0800

    Adding new codes

---
+++ src/pages/ai/question-answering.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <n-space vertical size="large">
+            <n-h1 prefix="bar">
+                <n-text type="primary"> 通用问答 </n-text>
+            </n-h1>
+            <QuestionInput />
+            <n-collapse-transition :show="gaokaoEssayStore.submissionStatus != 'NotSubmitted'">
+                <QuestionFeedback />
+            </n-collapse-transition>
+        </n-space>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useGaokaoEssayStore, Essay, EssayFeedback } from '@/stores/gaokaoEssay';
+
+const gaokaoEssayStore = useGaokaoEssayStore();
+</script>
